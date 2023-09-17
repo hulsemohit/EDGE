e8dcc48af31be49e0221160a67afc5d0025bf98f
Mohit Hulse <hulsemohit@gmail.com>
2023-09-17 00:24:53 -0400

    commands for building a container on modal

---
+++ modal_image.py
@@ -0,0 +1,30 @@
+import modal
+
+stub = modal.Stub("cygnus")
+
+image = (
+    modal.Image.debian_slim(python_version="3.11")
+    .apt_install("software-properties-common")
+    .apt_install("git")
+    .apt_install("wget")
+    .apt_install("curl")
+    .apt_install("ffmpeg")
+    .pip_install("youtube-dl")
+    .pip_install("wandb")
+    .pip_install("tqdm")
+    .pip_install("matplotlib")
+    .pip_install("soundfile")
+    .pip_install("librosa")
+    .pip_install("accelerate")
+    .pip_install("einops")
+    .pip_install("p_tqdm")
+    .pip_install("git+https://github.com/facebookresearch/pytorch3d.git")
+    .pip_install("git+https://github.com/rodrigo-castellon/jukemirlib.git")
+    .run_commands(["git clone https://github.com/hulsemohit/EDGE"])
+    .run_commands(["bash EDGE/download_model.sh"])
+    .run_commands(["mkdir EDGE/music"])
+)
+
+@stub.function(image=image,gpu="a100",memory=16384,cpu=8.0)
+def main():
+    pass
